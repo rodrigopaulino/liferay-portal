b8c006c38b6e07a112f6059c52f0fffa9fbbeae9
Kevin Tan <kevin.tan@liferay.com>
2016-09-14 17:49:51 -0700

    Search Results Map - Copy backend to new portlet

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/components/results/map/portlet/SearchResultsMapDisplayContext.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.components.results.map.portlet;
+
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.geolocation.GeoLocationPoint;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.search.web.internal.request.helper.OriginalHttpServletRequestSupplier;
+import com.liferay.portal.search.web.internal.request.helper.PortalOriginalHttpServletRequestSupplier;
+import com.liferay.portal.search.web.internal.request.helper.SearchHttpServletRequestHelper;
+import com.liferay.portal.search.web.internal.request.params.SearchParameters;
+import com.liferay.portal.search.web.internal.request.params.SearchParametersImpl;
+import com.liferay.portal.search.web.internal.results.data.SearchResultsData;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author André de Oliveira
+ */
+public class SearchResultsMapDisplayContext {
+
+	public SearchResultsMapDisplayContext(HttpServletRequest request) {
+		OriginalHttpServletRequestSupplier originalHttpServletRequestSupplier =
+			new PortalOriginalHttpServletRequestSupplier(
+				()->request, PortalUtil.getPortal());
+
+		_parameters = new SearchParametersImpl(
+			originalHttpServletRequestSupplier,
+			new SearchResultsMapConfigurationImpl());
+
+		_searchResultsData = SearchHttpServletRequestHelper.getResults(
+			originalHttpServletRequestSupplier);
+
+		_mapMarkersJSON = buildMapMarkersJSON();
+	}
+
+	public String getMapMarkersJSON() {
+		return _mapMarkersJSON;
+	}
+
+	public String getQ() {
+		return _parameters.getQParameter();
+	}
+
+	protected String buildMapMarkersJSON() {
+		JSONArray locations = JSONFactoryUtil.createJSONArray();
+
+		List<Document> documents = _searchResultsData.getDocuments();
+
+		documents.stream().flatMap(this::getMapMarkers).forEach(locations::put);
+
+		return locations.toString();
+	}
+
+	protected JSONObject getMapMarker(
+		GeoLocationPoint geoLocationPoint, String title, String summary) {
+
+		JSONObject jObj = JSONFactoryUtil.createJSONObject();
+
+		jObj.put("lat", geoLocationPoint.getLatitude());
+		jObj.put("lng", geoLocationPoint.getLongitude());
+		jObj.put("title", title);
+		jObj.put("summary", summary);
+
+		return jObj;
+	}
+
+	protected Stream<JSONObject> getMapMarkers(Document document) {
+		String title = document.get(Field.TITLE);
+		String summary = document.get(Field.CONTENT);
+
+		return document.getFields().values().stream()
+			.map(Field::getGeoLocationPoint).filter(Objects::nonNull)
+			.map(
+				geoLocationPoint-> getMapMarker(
+					geoLocationPoint, title, summary));
+	}
+
+	private final String _mapMarkersJSON;
+	private final SearchParameters _parameters;
+	private final SearchResultsData _searchResultsData;
+
+}
---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/components/results/map/portlet/SearchResultsMapPortlet.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.components.results.map.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.web.internal.request.helper.LiferayPortletHttpServletRequestSupplier;
+import com.liferay.portal.search.web.internal.request.helper.OriginalHttpServletRequestSupplier;
+import com.liferay.portal.search.web.internal.request.helper.PortalOriginalHttpServletRequestSupplier;
+import com.liferay.portal.search.web.internal.request.helper.SearchLiferayPortletRequestHelper;
+import com.liferay.portal.search.web.internal.request.params.SearchParameters;
+import com.liferay.portal.search.web.internal.request.params.SearchParametersImpl;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=" +
+			SearchResultsMapPortletKeys.CSS_CLASS_WRAPPER,
+		"com.liferay.portlet.display-category=category.search",
+		"com.liferay.portlet.icon=/icons/search.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.restore-current-view=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=" +
+			SearchResultsMapPortletKeys.DISPLAY_NAME,
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=" +
+			SearchResultsMapPortletKeys.VIEW_TEMPLATE,
+		"javax.portlet.name=" + SearchResultsMapPortletKeys.PORTLET_NAME,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class SearchResultsMapPortlet extends MVCPortlet {
+
+	// "javax.portlet.supported-public-render-parameter=q",
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		SearchParameters searchParameters = createSearchParameters(
+			renderRequest);
+
+		searchLiferayPortletRequestHelper.search(
+			searchParameters::getQParameter, renderRequest, renderResponse);
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	protected SearchParameters createSearchParameters(
+		RenderRequest renderRequest) {
+
+		OriginalHttpServletRequestSupplier originalHttpServletRequestSupplier =
+			new PortalOriginalHttpServletRequestSupplier(
+				new LiferayPortletHttpServletRequestSupplier(renderRequest),
+				portal);
+
+		return new SearchParametersImpl(
+			originalHttpServletRequestSupplier,
+			new SearchResultsMapConfigurationImpl());
+	}
+
+	@Reference
+	protected Portal portal;
+
+	@Reference
+	protected SearchLiferayPortletRequestHelper
+		searchLiferayPortletRequestHelper;
+
+}
