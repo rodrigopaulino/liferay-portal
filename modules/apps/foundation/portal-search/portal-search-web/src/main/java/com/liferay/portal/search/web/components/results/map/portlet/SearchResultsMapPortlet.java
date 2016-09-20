ddba4695384465a23dba6dae379f2b710d26b953
Kevin Tan <kevin.tan@liferay.com>
2016-09-19 17:50:53 -0700

    Search Results Map - Move css to separate file

--- modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/components/results/map/portlet/SearchResultsMapPortlet.java
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/components/results/map/portlet/SearchResultsMapPortlet.java
@@ -43,6 +43,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.css-class-wrapper=" +
 			SearchResultsMapPortletKeys.CSS_CLASS_WRAPPER,
 		"com.liferay.portlet.display-category=category.newsearch",
+		"com.liferay.portlet.header-portlet-css=/components/results/map/css/main.css",
 		"com.liferay.portlet.icon=/icons/search.png",
 		"com.liferay.portlet.instanceable=true",
 		"com.liferay.portlet.layout-cacheable=true",
