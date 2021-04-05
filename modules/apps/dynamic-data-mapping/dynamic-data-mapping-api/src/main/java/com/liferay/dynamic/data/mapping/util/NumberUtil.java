f38712194ad625200f75b39f9ec783a3f9752a44
Marcos <marcos.martins@liferay.com>
2021-04-05 15:44:34 -0300

    LPS-128822 create a number util to check if number has a decimal separator or not

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/NumberUtil.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+import java.util.Locale;
+
+/**
+ * @author Marcos Martins
+ */
+public class NumberUtil {
+
+	public static int getDecimalSeparatorIndex(Locale locale, String value) {
+		DecimalFormat decimalFormat = NumericDDMFormFieldUtil.getNumberFormat(
+			locale);
+
+		DecimalFormatSymbols decimalFormatSymbols =
+			decimalFormat.getDecimalFormatSymbols();
+
+		return value.indexOf(decimalFormatSymbols.getDecimalSeparator());
+	}
+
+	public static boolean hasDecimalSeparator(Locale locale, String value) {
+		if (getDecimalSeparatorIndex(locale, value) == -1) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
