5e55879594f9f1b0958049cd583fe8520de8d1a7
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-05-21 19:50:36 -0300

    LPS-132279 Upgrade existing expressions

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v2_0_1/DDMFormFieldValidationUpgradeProcess.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v2_0_1/DDMFormFieldValidationUpgradeProcess.java
@@ -14,94 +14,26 @@
 
 package com.liferay.dynamic.data.mapping.internal.upgrade.v2_0_1;
 
-import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.dynamic.data.mapping.internal.upgrade.util.BaseDDMStructureDefinitionUpgradeProcess;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Lino Alves
  */
-public class DDMFormFieldValidationUpgradeProcess extends UpgradeProcess {
+public class DDMFormFieldValidationUpgradeProcess
+	extends BaseDDMStructureDefinitionUpgradeProcess {
 
 	public DDMFormFieldValidationUpgradeProcess(JSONFactory jsonFactory) {
 		_jsonFactory = jsonFactory;
 	}
 
 	@Override
-	protected void doUpgrade() throws Exception {
-		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select structureId, definition from DDMStructure where " +
-					"classNameId = ? ");
-			PreparedStatement preparedStatement2 =
-				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-					connection,
-					"update DDMStructure set definition = ? where " +
-						"structureId = ?");
-			PreparedStatement preparedStatement3 = connection.prepareStatement(
-				"select structureVersionId, definition from " +
-					"DDMStructureVersion where structureId = ?");
-			PreparedStatement preparedStatement4 =
-				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-					connection,
-					"update DDMStructureVersion set definition = ? where " +
-						"structureVersionId = ?")) {
-
-			preparedStatement1.setLong(
-				1,
-				PortalUtil.getClassNameId(
-					"com.liferay.dynamic.data.mapping.model.DDMFormInstance"));
-
-			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
-				while (resultSet.next()) {
-					String definition = resultSet.getString("definition");
-
-					preparedStatement2.setString(
-						1, updateValidation(definition));
-
-					long structureId = resultSet.getLong("structureId");
-
-					preparedStatement2.setLong(2, structureId);
-
-					preparedStatement2.addBatch();
-
-					preparedStatement3.setLong(1, structureId);
-
-					try (ResultSet resultSet2 =
-							preparedStatement3.executeQuery()) {
-
-						while (resultSet2.next()) {
-							definition = resultSet2.getString("definition");
-
-							preparedStatement4.setString(
-								1, updateValidation(definition));
-
-							long structureVersionId = resultSet2.getLong(
-								"structureVersionId");
-
-							preparedStatement4.setLong(2, structureVersionId);
-
-							preparedStatement4.addBatch();
-						}
-					}
-				}
-			}
-
-			preparedStatement2.executeBatch();
-
-			preparedStatement4.executeBatch();
-		}
-	}
-
-	protected String updateValidation(String definition)
+	protected String upgradeDefinition(String definition)
 		throws PortalException {
 
 		JSONObject definitionJSONObject = _jsonFactory.createJSONObject(
