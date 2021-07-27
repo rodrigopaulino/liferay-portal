08816ae18592bd2decb6e60510c561ef020d938d
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2021-07-27 18:38:02 -0300

    LPS-135963 Use getLiferayPortletRequest instead of getUploadPortletRequest because PortletRequestImpl is a portal-impl internal class and using the ActionRequestFactory on integration tests would be unnecessarily complex

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/ImportDataDefinitionMVCActionCommand.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/ImportDataDefinitionMVCActionCommand.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
@@ -33,6 +34,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.upload.UploadPortletRequestImpl;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -63,7 +65,7 @@ public class ImportDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
 				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
 
 			UploadPortletRequest uploadPortletRequest =
-				_portal.getUploadPortletRequest(actionRequest);
+				_getUploadPortletRequest(actionRequest);
 
 			JSONObject jsonObject = _jsonFactory.createJSONObject(
 				FileUtil.read(uploadPortletRequest.getFile("jsonFile")));
@@ -107,6 +109,20 @@ public class ImportDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
 		}
 
 		sendRedirect(actionRequest, actionResponse);
+	}
+
+	private UploadPortletRequest _getUploadPortletRequest(
+		ActionRequest actionRequest) {
+
+		LiferayPortletRequest liferayPortletRequest =
+			_portal.getLiferayPortletRequest(actionRequest);
+
+		return new UploadPortletRequestImpl(
+			_portal.getUploadServletRequest(
+				liferayPortletRequest.getHttpServletRequest()),
+			liferayPortletRequest,
+			_portal.getPortletNamespace(
+				liferayPortletRequest.getPortletName()));
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
